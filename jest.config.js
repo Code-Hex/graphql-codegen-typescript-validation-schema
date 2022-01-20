1ae8238f72309ec2accaf13fd1e01cedd4cc7b26
Kei Kamikawa <code-hex@users.noreply.github.com>
2022-01-21 01:09:37 +0900

    added some tests

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+const { resolve } = require("path");
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+
+const pkg = require("./package.json");
+const tsconfig = require("./tsconfig.json");
+const CI = !!process.env.CI;
+
+module.exports = () => {
+  return {
+    displayName: pkg.name,
+    rootDir: __dirname,
+    preset: "ts-jest",
+    testEnvironment: "node",
+    restoreMocks: true,
+    reporters: ["default"],
+    modulePathIgnorePatterns: ["dist"],
+    moduleNameMapper: pathsToModuleNameMapper(
+      tsconfig.compilerOptions.paths || [],
+      {
+        prefix: `./`,
+      }
+    ),
+    cacheDirectory: resolve(
+      __dirname,
+      `${CI ? "" : "node_modules/"}.cache/jest`
+    ),
+    collectCoverage: false,
+  };
+};
---
+++ tests/yup.spec.ts
@@ -0,0 +1,237 @@
+// import "@graphql-codegen/testing";
+import { Types } from "@graphql-codegen/plugin-helpers";
+import { buildSchema } from "graphql";
+import { plugin } from "../src/index";
+
+describe("yup", () => {
+  test.each([
+    [
+      "required",
+      /* GraphQL */ `
+        input PrimitiveInput {
+          a: ID!
+          b: String!
+          c: Boolean!
+          d: Int!
+          e: Float!
+        }
+      `,
+      [
+        "export function PrimitiveInputSchema(): yup.SchemaOf<PrimitiveInput>",
+        "a: yup.string().required()",
+        "b: yup.string().required()",
+        "c: yup.boolean().required()",
+        "d: yup.number().required()",
+        "e: yup.number().required()",
+      ],
+    ],
+    [
+      "optional",
+      /* GraphQL */ `
+        input PrimitiveInput {
+          a: ID
+          b: String
+          c: Boolean
+          d: Int
+          e: Float
+          z: String! # no required check
+        }
+      `,
+      [
+        "export function PrimitiveInputSchema(): yup.SchemaOf<PrimitiveInput>",
+        // alphabet order
+        "a: yup.string(),",
+        "b: yup.string(),",
+        "c: yup.boolean(),",
+        "d: yup.number(),",
+        "e: yup.number(),",
+      ],
+    ],
+    [
+      "array",
+      /* GraphQL */ `
+        input ArrayInput {
+          a: [String]
+          b: [String!]
+          c: [String!]!
+          d: [[String]]
+          e: [[String]!]
+          f: [[String]!]!
+        }
+      `,
+      [
+        "export function ArrayInputSchema(): yup.SchemaOf<ArrayInput>",
+        "a: yup.array().of(yup.string()).optional(),",
+        "b: yup.array().of(yup.string().required()).optional(),",
+        "c: yup.array().of(yup.string().required()).required(),",
+        "d: yup.array().of(yup.array().of(yup.string()).optional()).optional(),",
+        "e: yup.array().of(yup.array().of(yup.string()).required()).optional(),",
+        "f: yup.array().of(yup.array().of(yup.string()).required()).required()",
+      ],
+    ],
+    [
+      "ref input object",
+      /* GraphQL */ `
+        input AInput {
+          b: BInput!
+        }
+        input BInput {
+          c: CInput!
+        }
+        input CInput {
+          a: AInput!
+        }
+      `,
+      [
+        "export function AInputSchema(): yup.SchemaOf<AInput>",
+        "b: yup.lazy(() => BInputSchema().required()) as never",
+        "export function BInputSchema(): yup.SchemaOf<BInput>",
+        "c: yup.lazy(() => CInputSchema().required()) as never",
+        "export function CInputSchema(): yup.SchemaOf<CInput>",
+        "a: yup.lazy(() => AInputSchema().required()) as never",
+      ],
+    ],
+    [
+      "nested input object",
+      /* GraphQL */ `
+        input NestedInput {
+          child: NestedInput
+          childrens: [NestedInput]
+        }
+      `,
+      [
+        "export function NestedInputSchema(): yup.SchemaOf<NestedInput>",
+        "child: yup.lazy(() => NestedInputSchema()) as never,",
+        "childrens: yup.array().of(yup.lazy(() => NestedInputSchema()) as never).optional()",
+      ],
+    ],
+    [
+      "enum",
+      /* GraphQL */ `
+        enum PageType {
+          PUBLIC
+          BASIC_AUTH
+        }
+        input PageInput {
+          pageType: PageType!
+        }
+      `,
+      [
+        "export const PageTypeSchema = yup.mixed().oneOf([PageType.Public, PageType.BasicAuth])",
+        "export function PageInputSchema(): yup.SchemaOf<PageInput>",
+        "pageType: PageTypeSchema.required()",
+      ],
+    ],
+    [
+      "camelcase",
+      /* GraphQL */ `
+        input HTTPInput {
+          method: HTTPMethod
+          url: URL!
+        }
+
+        enum HTTPMethod {
+          GET
+          POST
+        }
+
+        scalar URL # unknown scalar, should be any (yup.mixed())
+      `,
+      [
+        "export function HttpInputSchema(): yup.SchemaOf<HttpInput>",
+        "export const HttpMethodSchema = yup.mixed().oneOf([HttpMethod.Get, HttpMethod.Post])",
+        "method: HttpMethodSchema",
+        "url: yup.mixed().required()",
+      ],
+    ],
+  ])("%s", async (_, textSchema, wantContains) => {
+    const schema = buildSchema(textSchema);
+    const result = await plugin(schema, [], {}, {});
+    expect(result.prepend).toContain("import * as yup from 'yup'");
+
+    for (const wantContain of wantContains) {
+      expect(result.content).toContain(wantContain);
+    }
+  });
+
+  it("with scalars", async () => {
+    const schema = buildSchema(/* GraphQL */ `
+      input Say {
+        phrase: Text!
+        times: Count!
+      }
+
+      scalar Count
+      scalar Text
+    `);
+    const result = await plugin(
+      schema,
+      [],
+      {
+        scalars: {
+          Text: "string",
+          Count: "number",
+        },
+      },
+      {}
+    );
+    expect(result.content).toContain("phrase: yup.string().required()");
+    expect(result.content).toContain("times: yup.number().required()");
+  });
+
+  it("with importFrom", async () => {
+    const schema = buildSchema(/* GraphQL */ `
+      input Say {
+        phrase: String!
+      }
+    `);
+    const result = await plugin(
+      schema,
+      [],
+      {
+        importFrom: "./types",
+      },
+      {}
+    );
+    expect(result.prepend).toContain("import { Say } from './types'");
+    expect(result.content).toContain("phrase: yup.string().required()");
+  });
+
+  it("with enumsAsTypes", async () => {
+    const schema = buildSchema(/* GraphQL */ `
+      enum PageType {
+        PUBLIC
+        BASIC_AUTH
+      }
+    `);
+    const result = await plugin(
+      schema,
+      [],
+      {
+        enumsAsTypes: true,
+      },
+      {}
+    );
+    expect(result.content).toContain(
+      "export const PageTypeSchema = yup.mixed().oneOf(['PUBLIC', 'BASIC_AUTH'])"
+    );
+  });
+  // describe("with importFrom", () => {
+  //   it("Should work", async () => {
+  //     const schema = buildSchema(/* GraphQL */ `
+  //       "custom enum"
+  //       enum MyEnum {
+  //         "this is a"
+  //         A
+  //         "this is b"
+  //         B
+  //       }
+  //     `);
+  //     const result = (await plugin(schema, [], {
+  //       importFrom: "./generated-types",
+  //     })) as Types.ComplexPluginOutput;
+
+  //     expect(1).toBe(1)
+  //   });
+  // });
+});
