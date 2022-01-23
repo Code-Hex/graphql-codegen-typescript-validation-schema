69e043958c11e4b402d9b7f6b980f7a9a303574d
Kei Kamikawa <code-hex@users.noreply.github.com>
2022-01-23 18:31:51 +0900

    added directive.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,16 @@ import { TypeScriptPluginConfig } from "@graphql-codegen/typescript";
 
 export type ValidationSchema = "yup";
 
+export interface DirectiveConfig {
+  [directive: string]: {
+    [argument: string]: string | string[] | DirectiveObjectArguments;
+  };
+}
+
+export interface DirectiveObjectArguments {
+  [matched: string]: string | string[];
+}
+
 export interface ValidationSchemaPluginConfig extends TypeScriptPluginConfig {
   /**
    * @description specify generate schema
@@ -66,7 +76,48 @@ export interface ValidationSchemaPluginConfig extends TypeScriptPluginConfig {
   /**
    * @description this is for yup schema. use this when you specified `schema: yup`
    */
-  yup?: YupSchemaPluginConfig
+  yup?: YupSchemaPluginConfig;
+  /**
+   * @description Generates yup schema as strict.
+   * @exampleMarkdown
+   * ```yml
+   * generates:
+   *   path/to/file.ts:
+   *     plugins:
+   *       - graphql-codegen-validation-schema:
+   *     config:
+   *       schema: yup
+   *       directives:
+   *         required:
+   *           msg: required
+   *         # This is example using constraint directive.
+   *         # see: https://github.com/confuser/graphql-constraint-directive
+   *         constraint:
+   *           minLength: min # same as ['min', '$1']
+   *           maxLength: max
+   *           startsWith: ["matches", "^$1"]
+   *           endsWith: ["matches", "$1$"]
+   *           contains: ["matches", "$1"]
+   *           notContains: ["matches", "^((?!$1).)*$"]
+   *           pattern: matches
+   *           format:
+   *             # For example, `@constraint(format: "uri")`. this case $1 will be "uri".
+   *             # Therefore the generator generates yup schema `.url()` followed by `uri: 'url'`
+   *             # If $1 does not match anywhere, the generator will ignore.
+   *             uri: url
+   *             email: email
+   *             uuid: uuid
+   *             # yup does not have `ipv4` API. If you want to add this,
+   *             # you need to add the logic using `yup.addMethod`.
+   *             # see: https://github.com/jquense/yup#addmethodschematype-schema-name-string-method--schema-void
+   *             ipv4: ipv4
+   *           min: ["min", "$1 - 1"]
+   *           max: ["max", "$1 + 1"]
+   *           exclusiveMin: min
+   *           exclusiveMax: max
+   * ```
+   */
+  directives?: DirectiveConfig;
 }
 
 interface YupSchemaPluginConfig {
@@ -86,5 +137,5 @@ interface YupSchemaPluginConfig {
    *         strict: true
    * ```
    */
-  strict?: boolean
+  strict?: boolean;
 }
