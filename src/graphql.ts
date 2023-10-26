b94e48da6e9cf4e5c66d1ebb086c5caaf1cefe0f
wand2016 <d19921207@gmail.com>
2023-10-26 18:58:54 +0900

    make required the arguments for type guards

--- src/graphql.ts
+++ src/graphql.ts
@@ -14,9 +14,9 @@ import {
   visit,
 } from 'graphql';
 
-export const isListType = (typ?: TypeNode): typ is ListTypeNode => typ?.kind === 'ListType';
-export const isNonNullType = (typ?: TypeNode): typ is NonNullTypeNode => typ?.kind === 'NonNullType';
-export const isNamedType = (typ?: TypeNode): typ is NamedTypeNode => typ?.kind === 'NamedType';
+export const isListType = (typ: TypeNode): typ is ListTypeNode => typ.kind === 'ListType';
+export const isNonNullType = (typ: TypeNode): typ is NonNullTypeNode => typ.kind === 'NonNullType';
+export const isNamedType = (typ: TypeNode): typ is NamedTypeNode => typ.kind === 'NamedType';
 
 export const isInput = (kind: string) => kind.includes('Input');
 
