e99fe70796d7241ac70638bc39937bee13146522
wand2016 <d19921207@gmail.com>
2023-11-02 12:25:21 +0900

    fix testing import

--- src/TsValidationMethodCallMapper.spec.ts
+++ src/TsValidationMethodCallMapper.spec.ts
@@ -1,3 +1,5 @@
+import { describe, expect, test } from 'vitest';
+
 import { TsValidationMethodCallMapper } from './TsValidationMethodCallMapper';
 
 describe('TsValidationMethodCallMapper', () => {
