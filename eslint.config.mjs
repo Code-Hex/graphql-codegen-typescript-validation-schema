e0a1cd9ce0c3a4dbec43d534310d75dfee1c1eca
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2024-04-06 13:48:29 +0900

    removed prettier and enable @antfu/eslint-config

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  typescript: true,
+  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+  ignores: [
+    'dist/**',
+    'node_modules/**',
+    'example/**',
+    'vitest.config.ts',
+    'tsconfig.json',
+  ],
+})
