a79e9432fdcda7411a0e3342b2c5c61dffef6c2d
MPiland <piland.mark@gmail.com>
2025-06-05 10:36:33 -0500

    Update index.ts

--- src/myzod/index.ts
+++ src/myzod/index.ts
@@ -238,7 +238,7 @@ export class MyZodSchemaVisitor extends BaseSchemaVisitor {
   ) {
     const typeName = visitor.prefixTypeNamespace(name);
     const discriminator =
-      this.config.inputDiscriminator ? `${this.config.inputDiscriminator}: z.literal('${name}',\n)` : ''
+      this.config.inputDiscriminator ? `${this.config.inputDiscriminator}: z.literal('${name}'),\n` : ''
     const shape = fields.map(field => generateFieldMyZodSchema(this.config, visitor, field, 2)).join(',\n');
 
     switch (this.config.validationSchemaExportType) {
